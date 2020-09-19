968a6596d6d5a185a2d0419b4e01d21af0813226
nat <nmckee9@gmail.com>
2020-09-19 14:31:36 -0700

    edits about section, add font styling

--- nlmportfolio/src/components/About/about.js
+++ nlmportfolio/src/components/About/about.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from '../../images/profile.jpg'; 
 import "./style.css"
 
 function About() {
@@ -10,25 +9,25 @@ function About() {
   <div class="column is-4 has-text-centered">
   <div class="">
   <p class="title is-small abouttitletext">
-  Wildly Curious
+  Passionate Learner
     </p>
-    Never-Ending Quest for New Places and Knowledge
     <p />
+     <p class="subtitle subtitletext">  Continually Seeking New Skills  </p>
     </div>
   </div>
   <div class="column is-4 has-text-centered">
   <p class="title is-small abouttitletext">
-  Passionate Learner
+  Wildly Curious
     </p>
-    Continually Seeking New Skills 
-      <p />
+    <p />
+    <p class="subtitle subtitletext"> Never-Ending Quest for New Places and Knowledge </p>
   </div>
   <div class="column is-4 has-text-centered">
   <p class="title is-small abouttitletext">
   Human Connection
     </p>
-    Thrive on People Interactions and Connections
     <p />
+   <p class="subtitle subtitletext">Thrive on People Interactions and Connections</p> 
   </div>
 </div>
 </div>
